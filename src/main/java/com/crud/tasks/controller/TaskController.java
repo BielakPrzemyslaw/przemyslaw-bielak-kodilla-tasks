dac95c23b701c6598c89b20e09ba722052028821
BielakPrzemyslaw <przemyslawbielak@interia.pl>
2020-04-13 17:46:20 +0200

    TaskControler

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -3,34 +3,45 @@ package com.crud.tasks.controller;
 import com.crud.tasks.domain.TaskDto;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping(method = RequestMethod.GET, value = "getTask")
+@RequestMapping("/v1/task")
 public class TaskController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "getTask")
     public List<TaskDto> getTasks() {
         return new ArrayList<>();
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/tasks/{taskId}")
-    public TaskDto getTask(Long taskId) {
-        return new TaskDto(1L, "test title", "test content");
+    @RequestMapping(method = RequestMethod.GET,
+            value = "getTask",
+            params = "taskId")
+    public TaskDto getTask(@RequestParam("taskId") String taskId) {
+        return new TaskDto((long)1, "test title", "test content");
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "/tasks/{taskId}")
-    public void deleteTask(Long taskId) {
+    @RequestMapping(method = RequestMethod.DELETE,
+            params = "taskId")
+    public void deleteTask(@RequestParam("taskId") String taskId) {
 
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "/tasks")
-    public TaskDto updateTask(TaskDto taskDto) {
-        return new TaskDto(1L, "Edited test title", "Test content");
+    @RequestMapping(method = RequestMethod.POST,
+            value = "updateTask",
+            params = "taskDto")
+    public TaskDto updateTask(@RequestParam("taskDto") TaskDto taskDto) {
+        return new TaskDto((long)1, "Edited test title", "Test content");
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/tasks")
-    public void createTask(TaskDto taskDto) {
+    @RequestMapping(method = RequestMethod.PUT,
+            value = "createTask",
+            params = "taskDto")
+    public void createTask(@RequestParam("taskDto") TaskDto taskDto) {
 
     }
 }
