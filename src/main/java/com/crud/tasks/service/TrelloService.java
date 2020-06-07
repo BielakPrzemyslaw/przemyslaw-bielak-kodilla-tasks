63b4dcb5bed6b43bf5e908587c7e0602c7d525f2
BielakPrzemyslaw <przemyslawbielak@interia.pl>
2020-06-07 11:42:45 +0200

    Change

--- src/main/java/com/crud/tasks/service/TrelloService.java
+++ src/main/java/com/crud/tasks/service/TrelloService.java
@@ -34,7 +34,7 @@ public class TrelloService {
     public CreatedTrelloCard createdTrelloCard(final TrelloCardDto trelloCardDto) {
         CreatedTrelloCard newCard = trelloClient.createNewCard(trelloCardDto);
         Optional.ofNullable(newCard).ifPresent(card -> emailService.send(new Mail(adminConfig.getAdminMail(), SUBJECT,
-                "New card: " + card.getName() + " has been created on your Trello account", "")));
+                "New card: " + card.getName() + " has been created on your Trello account")));
         return newCard;
     }
 
